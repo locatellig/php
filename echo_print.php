59083a28baa7a4884e38b75fadcfc8acf4c609d7
locatellig <guilhermeloka12@gmail.com>
2024-11-28 22:15:13 -0300

    Primeiro commit estudos php

---
+++ echo_print.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curso PHP</title>
+    </head>
+
+    <body>
+        <!-- Impressão de conteúdos do bloco php no arquivo html -->
+        <?php
+            echo 'Comando echo<br>';
+            print 'Comando print<hr>';
+            echo print 'Comando echo print, se deu certo = 1, errado = 0<hr>';
+        ?>     
+        
+    </body>
+</html>
